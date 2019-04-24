3b09181e1e08d7886e6bd761023bc37365d722a2
gianlvalentini <gian.valentini1997@gmail.com>
2019-04-24 17:15:34 -0300

    adding hilos library

---
+++ BibliotecaCompartida/biblioteca/hilos.c
@@ -0,0 +1,26 @@
+#include "hilos.h"
+
+pthread_t crearHilo(void* funcion, void* param){
+
+	pthread_t nuevoHilo;
+
+	pthread_create(&nuevoHilo, NULL, funcion, param);
+
+	return nuevoHilo;
+
+}
+
+void crearHiloDetachable(void* funcion, void* param){
+
+	pthread_t nuevoHilo;
+	pthread_attr_t attr;
+
+	pthread_attr_init(&attr);
+	pthread_attr_setdetachstate(&attr, PTHREAD_CREATE_DETACHED);
+	pthread_create(&nuevoHilo, &attr, funcion, param);
+
+}
+
+int esperarHilo(pthread_t hilo){
+	return pthread_join(hilo, NULL);
+}
---
+++ BibliotecaCompartida/biblioteca/hilos.h
@@ -0,0 +1,13 @@
+#ifndef HILOS_H_
+#define HILOS_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <pthread.h>
+
+pthread_t crearHilo(void* funcion, void* param);
+void crearHiloDetachable(void* funcion, void* param);
+int esperarHilo(pthread_t hilo);
+
+#endif /* HILOS_H_ */
