19796dbebf3b9b431137c0830bf5798b7fb6eccf
Mariano <mariano.ingrassia.91@gmail.com>
2019-05-26 23:05:25 -0300

    Lo que hay hasta ahora del kernel

---
+++ Kernel/src/kernel_api.c
@@ -0,0 +1,17 @@
+/*
+ * kernel_api.c - API del kernel
+ */
+
+
+#include<stdio.h>
+/*
+void main(int argc, char *argv[])
+{
+}
+*/
+
+void journal_request()
+{
+	printf("Journaling solicitado con exito");
+}
+
